e545331eef69b165dcf9c3fb08dad095bb512a98
atmshnko2gis <a.timoshenko@2Gis.ru~>
2026-03-12 12:49:56 +0300

    update

--- bot/main.py
+++ bot/main.py
@@ -3,9 +3,11 @@ from aiogram import Bot, Dispatcher
 from .config import BOT_TOKEN
 from .handlers import basic, votes
 from .scheduler import setup_scheduler
+from .database import init_db
 
 
 async def main():
+    init_db()
     bot = Bot(token=BOT_TOKEN, validate_token=False)
     dp = Dispatcher()
 
