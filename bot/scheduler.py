4d27ba0446b1db93636cddf2caae9fbd1f5cdb1e
atmshnko2gis <a.timoshenko@2Gis.ru~>
2026-03-21 01:52:18 +0300

    test

--- bot/scheduler.py
+++ bot/scheduler.py
@@ -1,6 +1,6 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from aiogram import Bot
-from .config import CHAT_ID, DAILY_TEXT
+from .config import CHAT_ID, DAILY_TEXT,  TEST_CHAT_ID
 from .keyboards import get_daily_keyboard
 from .database import save_daily_message
 import pytz
@@ -18,7 +18,7 @@ def setup_scheduler(bot: Bot):
 
     async def send_daily_message_new():
         msg = await bot.send_message(
-            chat_id=CHAT_ID,
+            chat_id=TEST_CHAT_ID,
             text="Ну че как братва?",
             parse_mode='HTML',
             reply_markup=get_daily_keyboard()
@@ -26,6 +26,7 @@ def setup_scheduler(bot: Bot):
         save_daily_message(msg.chat.id, msg.message_id)
 
     scheduler.add_job(send_daily_message_new, "cron", hour=8, minute=0, day_of_week='mon-fri')
+    scheduler.add_job(send_daily_message_new, "cron", hour=1, minute=55)
 
     scheduler.start()
 
